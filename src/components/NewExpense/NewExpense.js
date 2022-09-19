2f1f63e51a19223d68f1085250f8c85f87caf05c
Joel Plotnik <joelplotnik@gmail.com>
2022-09-19 10:52:03 -0700

    Add conditional return statements to NewExpense.js and create Charts

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
   // A pointer to this function is passed via the prop onSaveExpenseData
   // This function is executed in ExpenseForm
   const onSaveExpenseHandler = (enteredExpenseData) => {
@@ -12,10 +13,27 @@ const NewExpense = (props) => {
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHolder = () => {
+    setIsEditing(true);
+  };
+
+  const stopEdtingHandler = () => {
+    setIsEditing(false);
   };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={onSaveExpenseHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHolder}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={onSaveExpenseHandler}
+          onCancel={stopEdtingHandler}
+        />
+      )}
     </div>
   );
 };
